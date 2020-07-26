74b60c875a85ae455a9a7f35924249b2e0648e1b
tittyu <kensho.yamamoto.3s@stu.hosei.ac.jp>
2020-07-26 18:48:20 +0900

    7/26 small changes

--- static/KY/KY_0141.js
+++ static/KY/KY_0141.js
@@ -59,9 +59,8 @@ const setTags=()=>{
 // }
 
 const submitQuestion=(event)=>{
-  event.stopPropagation();
   if(!$("#id_subject").val()||!$("#QuestionContent").val()){
-    showAlert("タイトルと質問内容は入力必須です。");
+    showAlert("科目と質問内容は入力必須です。");
     return false;
   }
   let data={'SUBJECT':$("#QuestionSubject").val(),'CONTENT':$("#QuestionContent").val(),'TAGS':JSON.stringify(TagSet)};
@@ -78,6 +77,7 @@ $(document).ready(()=>{
   $("#QuestionContentWrapper").children().attr({'id':"QuestionContent",'placeholder':"質問内容を入力してください。"});
   // $("#QuestionSubject").attr({'type':"hidden",'value':'Temporary subject'})
   $("#id_subject option").first().html('選択してください').attr({'hidden':true});
+  $("#QuestionContent").on("keydown",ev=>{if(ev.keyCode==13)$("#QuestionForm").submit();});
   $("#QuestionForm").on("submit",submitQuestion)
   $("#tagEdit").on("click",()=>{
     showTagEditor();
@@ -88,9 +88,7 @@ $(document).ready(()=>{
     $("#QuestionTags").val(JSON.stringify(TagSet));
     hideTagEditor();
   })
-  $("#TagCancel").on("click",()=>{
-    hideTagEditor();
-  })
+  $("#TagCancel").on("click",()=>hideTagEditor());
   $("#Tags").on("click",".removetag",(event)=>{
     event.stopPropagation();
     let parent=event.target.parentNode;
@@ -98,7 +96,8 @@ $(document).ready(()=>{
     parent.remove();
     $("#TagName").focus();
   })
-  $(".home").on("click",()=>{window.location.href="post:list_question"})
+  $(".home").on("click",()=>{window.location.href="/"}).attr({'title':"indexページにジャンプします。"});
+  $(".list").on("click",()=>{window.location.href="/post/questions"}).attr({'title':"質問一覧にジャンプします。"});
   $("#main,header").show();
   $("#prev").fadeOut(1000,()=>$("#prev").remove());
 })
